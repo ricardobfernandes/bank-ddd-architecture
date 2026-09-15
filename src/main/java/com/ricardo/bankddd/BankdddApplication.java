11e1a5d5c88b4807d6bf9f7a65a67f27c0445b0b
Ricardo Fernandes <ricardo.b.fernandes@accenture.com>
2026-09-15 14:17:29 -0300

    Project Created

---
+++ src/main/java/com/ricardo/bankddd/BankdddApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.bankddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankdddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankdddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardo/bankddd/BankdddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.bankddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankdddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
